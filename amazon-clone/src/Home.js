e27733bef1d1b85d3b0346e09521f9f485ab6d2b
Jake Grella <jgrella13@gmail.com>
2020-09-20 16:05:42 -0500

    finished banner image

---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return <div className="home">
+        <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MWJkMjYwNzAt/MWJkMjYwNzAt-NGVmZmRlNjEt-w1500._CB405404417_.jpg" alt="banner image of tv" />
+    </div>
+}
+
+export default Home
